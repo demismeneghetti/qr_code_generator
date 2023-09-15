4f30f6026d60b1396d7a0312d4675280bac78a47
Demis Meneghetti <demismeneghetti@msn.com>
2023-09-15 17:14:25 +0100

    Finished

---
+++ generator.py
@@ -0,0 +1,18 @@
+import pyqrcode
+import datetime
+import os
+
+current_date_time = datetime.datetime.now()
+format_current_date_time = current_date_time.strftime("%Y%m%d_%H%M%S")
+
+directory_path = ".\\qr_code_generator\\generated_qr_codes"
+file_name = f"{directory_path}\\qrcode_{format_current_date_time}.png"
+
+os.makedirs(directory_path, exist_ok=True)
+
+qr = pyqrcode.create(input("Enter the desired URL:\n"))
+qr.png(file_name, scale=6)
+
+print("-------------------------------")
+print("QR Code Generated successfully.")
+print("-------------------------------")
